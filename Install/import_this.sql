ddc9f9f276995775911eb8f76f31150380bfda6c
scott lindh <scottlindh@gmail.com>
2014-05-12 09:11:07 -0700

    SQL script for installation of LimeCart
    Installation SQL script to run while installing lime cart, this will
    create a table in chosen database and then populate it with 2 test
    products for you.

---
+++ Install/import_this.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_code` varchar(60) NOT NULL,
+  `product_name` varchar(60) NOT NULL,
+  `product_desc` tinytext NOT NULL,
+  `product_img_name` varchar(60) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `product_code` (`product_code`)
+) AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `product_code`, `product_name`, `product_desc`, `product_img_name`, `price`) VALUES
+(1, 'PD1001', 'Some Phone', '', 'Ia menggunakan kamus yang mengandungi lebih 200 ayat Latin, bersama model dan struktur ayat Latin, untuk menghasilkan Lorem Ipsum yang munasabah','scratchcard.jpg', 200.50),
+(2, 'PD1002', 'Some TV', 'Ia menggunakan kamus yang mengandungi lebih 200 ayat Latin, bersama model dan struktur ayat Latin, untuk menghasilkan Lorem Ipsum yang munasabah.', 'usbcard.jpg', 500.85),
+
